f281086168f4f6d718514de1e91348777e486bb1
Bence Szabo <beszabo@protonmail.com>
2023-04-25 01:24:09 +0200

    adding publishable key to clerkprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,11 @@ import {ClerkProvider} from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider {...pageProps} publishableKey={publishableKey} >
       <Component {...pageProps} />
     </ClerkProvider>
   );
